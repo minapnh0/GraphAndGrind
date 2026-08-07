abe922726304118369ea5cd8aad7dc3bce550415
Mina <panahimn@gmail.com>
2026-08-06 23:52:23 -0500

    mutable

---
+++ 02_datatyeps/chapter_1.py
@@ -0,0 +1,8 @@
+sugar_amaount= 2
+print (f"intial sugar: {sugar_amaount}")
+
+sugar_amaount= 12
+print (f"second sugar: {sugar_amaount}")
+
+print (f" ID of 2:{id(2)}")
+print (f" ID of 12 :{id(12)}")]]
---
+++ 02_datatyeps/chapter_2.py
@@ -0,0 +1,7 @@
+spice_mix = set()
+print(f"initial spice mix: {id(spice_mix)}")
+
+spice_mix.add("cinnamon")
+spice_mix.add("cardamom")
+
+print(f"spice mix after adding spices: {id(spice_mix)}")
