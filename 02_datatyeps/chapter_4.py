5cc155514b997d4f1c9a486d9612b08aa6b2b141
Mina <panahimn@gmail.com>
2026-08-08 15:39:54 -0500

    Add chapter_4.py with initial implementation of boiling and milk presence checks

---
+++ 02_datatyeps/chapter_4.py
@@ -0,0 +1,10 @@
+is_boiling= True
+stri_count = 6
+total_actions = is_boiling + stri_count # Upcasting
+print (f"Total actions {total_actions}")
+
+milk_present = 0
+print (f"Is there milk? {bool(milk_present)}")
+
+milk_presented = "milk"
+print (f"Is there milk? {bool(milk_presented)}")
