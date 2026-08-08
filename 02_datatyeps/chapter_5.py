f1a0297e9728ccf0d16d687dca434515248adc59
Mina <panahimn@gmail.com>
2026-08-08 18:05:23 -0500

    Add chapter_5.py to display ideal and current temperature with difference

---
+++ 02_datatyeps/chapter_5.py
@@ -0,0 +1,10 @@
+import sys
+
+ideal_temp= 95.5
+current_temp = 95.4999999999999
+
+print (f"Ideal temp {ideal_temp}")
+print (f"Ideal temp {current_temp}")
+
+print (f"Difference between temp {ideal_temp - current_temp} ")
+print(sys.float_info)
