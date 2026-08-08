860d249198d4cbe1e8925571b193108e0e91776c
Mina <panahimn@gmail.com>
2026-08-08 15:31:45 -0500

    numbers

---
+++ 02_datatyeps/chapter_3.py
@@ -0,0 +1,33 @@
+# Integer
+
+black_tea_grams = 14
+sugar_grams= 3 
+
+total_grams = black_tea_grams + sugar_grams
+print (f"Total grams of tea {total_grams}")
+
+
+remaining_tea = black_tea_grams - sugar_grams
+print(f"Total grams of black tea {remaining_tea}")
+
+
+tea_bag = 17
+serving = 4
+tea_per_serving = tea_bag/serving
+print(f"milk per serving {tea_per_serving}")
+tea_bag_serving = tea_bag//serving
+print(f"tea bag per serving {tea_bag_serving}")
+
+total_cadamom_pods = 10
+podes_per_pods = 3
+leftover_pods = total_cadamom_pods %podes_per_pods
+print (f"leftover pods {leftover_pods}")
+
+base_flavor_streanght = 2
+scale_factor = 3
+powerful_flavour = base_flavor_streanght ** scale_factor
+print (f" Scaled flavour tea {powerful_flavour}")
+
+total_tea_leaves_harvested = 1_000_000_000
+print (f"Total tea harveste {total_tea_leaves_harvested}")
+
