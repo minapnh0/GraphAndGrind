a75a2ce78616bcd38800c7a07db5c01fd2a2fd64
Mina <panahimn@gmail.com>
2026-08-08 22:58:42 -0500

    array and list implementations

---
+++ chapter_8.py
@@ -0,0 +1,45 @@
+#Array , List, Mutable
+
+ingredients = ["water", "milk", "black tea"]
+ingredients.append("sugar")
+print(f"Ingredients are: {ingredients}")
+
+ingredients.remove("water")
+print(f"Ingredients are: {ingredients}")
+
+spice_options = ["ginger", "cardamom"]
+chai_ingredients = ["water", "milk"]
+
+chai_ingredients.extend(spice_options)
+print(f"Chai Ingredients are: {chai_ingredients}")
+
+chai_ingredients.insert(2,"black tea")
+print(f"Chai Ingredients are: {chai_ingredients}")
+chai_ingredients.remove("black tea")
+print(f"Chai Ingredients are: {chai_ingredients}")
+chai_ingredients.insert(3,"green tea")
+print(f"Chai Ingredients are: {chai_ingredients}")
+
+last_added = chai_ingredients.pop()
+print(f"what was last added {last_added}")
+print(f"Chai Ingredients are: {chai_ingredients}")
+chai_ingredients.insert(4,"black tea")
+print(f"Chai Ingredients are: {chai_ingredients}")
+chai_ingredients.reverse()
+print(f"chai: {chai_ingredients}")
+
+sugar_levels = [1, 2, 3, 4, 5]
+print(f"Max sugar level: {max(sugar_levels)}")
+print(f"Min sugar level: {min(sugar_levels)}")
+
+base_liquid = ["water", "milk"]
+extra_flavor = ["ginger"]
+
+full_liquid_mix = base_liquid + extra_flavor
+print(f"Liquid mix: {full_liquid_mix}")
+
+strong_brew = ["black tea", "water"] * 3
+print(f"String brew: {strong_brew}")
+
+
+
