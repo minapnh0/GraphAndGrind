e4ecd907023e97c899a5e12b9a2ef21126cf83b1
Mina <panahimn@gmail.com>
2026-08-08 22:39:12 -0500

    Tuples

--- 02_datatyeps/chapter_7.py
+++ 02_datatyeps/chapter_7.py
@@ -1,4 +1,16 @@
 #Tuples -Immutable
 
+spices = ("salt", "cloves", "safron")
+(spice1, spice2, spice3) = spices
+print(f"Main Spices: {spice1}, {spice2}, {spice3}")
 
+ginger_ratio , salt_ratio = 2,3
+print(f"Ratio Ginger is: {ginger_ratio}, Ratio salt is:{salt_ratio}")
 
+ginger_ratio , salt_ratio = salt_ratio, ginger_ratio
+print(f"Ratio Ginger is: {ginger_ratio}, Ratio salt is:{salt_ratio}")
+
+# membership testing
+(print(f"Is safron is in spices? {"safron" in spices}"))
+(print(f"Is paprica is in spices? {"paprica" in spices}"))
+print(f"Is cloves in spoces? {"cloves" in spices}")
