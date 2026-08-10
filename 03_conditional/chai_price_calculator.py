8dcdffe0b48f00b59128c0b57cba4ea731c66794
Mina <panahimn@gmail.com>
2026-08-09 23:15:32 -0500

    Fix variable name in chai price calculator for consistency

--- 03_conditional/chai_price_calculator.py
+++ 03_conditional/chai_price_calculator.py
@@ -1,10 +1,10 @@
 cup_size = input("Choose your cup size (small/medium/large): ").lower()
 
-if cup =="small":
+if cup_size =="small":
      print("Price is 10 rupees")
-elif cup == "medium":
+elif cup_size == "medium":
      print("Price is 15 rupees")
-elif cup == "large":
+elif cup_size == "large":
     print("price is 20 rupees")
 else:
      print("Unknown cup size")
