7df55f250af6929f8351e2b410f4b2ab71e04595
Mina <panahimn@gmail.com>
2026-08-13 23:17:47 -0500

    Add print statement for available chai flavors

--- 04_loops/09_walrus.py
+++ 04_loops/09_walrus.py
@@ -19,8 +19,10 @@ if remainder:= value % 5:
 
 # ------
 flavors = ["masala", "ginger", "lemon", "mint"]
+print("Available flavors: ", flavors)
 while (flavor := input("Choose your flavor: ")) not in flavors:
     print(f"Sorry, {flavor} is not available")
+
 print(f"You choose {flavor} chai")
 
 
